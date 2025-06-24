baf5522a822883fb9dbeb6c35400243dadb74e6b
benjaminelsw <be.quintriqueo@duocuc.cl>
2025-06-24 16:43:47 -0400

    Initial commit

---
+++ src/test/java/ecomarket/duoc/ecomarket/duoc/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ecomarket.duoc.ecomarket.duoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
